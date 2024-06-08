97d8aa531a88971563a17fdfb8addc21b759cb25
波と一緒に漂う <2247564152@qq.com>
2024-06-09 03:27:25 +0800

    update

---
+++ lua/autorun/cso2hasan.lua
@@ -0,0 +1,11 @@
+player_manager.AddValidModel( "CSO2Hasan", "models/cso2/pm/hasanpm.mdl" )
+
+list.Set( "PlayerOptionsModel", "CSO2Hasan", "models/cso2/pm/hasanpm.mdl" )
+
+player_manager.AddValidModel( "CSO2Hasan2", "models/cso2/pm/hasanpm2.mdl" )
+
+list.Set( "PlayerOptionsModel", "CSO2Hasan2", "models/cso2/pm/hasanpm2.mdl" )
+
+player_manager.AddValidHands( "CSO2Hasan", "models/cso2/pm/hasanhand.mdl", 0, "0" )
+
+player_manager.AddValidHands( "CSO2Hasan2", "models/cso2/pm/hasanhand.mdl", 0, "0" )
